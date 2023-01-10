f36bea42be7c93128bbaafe78036bbb0d15b92d8
mimseyedi <mim.seyedi@gmail.com>
2023-01-10 13:02:09 +0330

    Added functions g2j and j2g to convert Gregorian date to Jalali and Jalali date to Gregorian
    These functions are obtained from: https://jdf.scr.ir/jdf/python

--- src/jaldt/jalali_datetime.py
+++ src/jaldt/jalali_datetime.py
@@ -101,3 +101,101 @@ class JalaliIntegerMonth(int, Enum):
     dey = 10
     bahman = 11
     esfand = 12
+
+
+def g2j(gy: int, gm: int, gd: int) -> List[int]:
+    """
+    Convert Gregorian date to Jalali.
+    The function to convert the Gregorian date to Jalali is taken from the following link:
+    https://jdf.scr.ir/jdf/python
+
+    :param gy: Gregorian year: int
+    :param gm: Gregorian month: int
+    :param gd: Gregorian day: int
+    :return: List[jalali_year: int, jalali_month: int, jalali_day: int]
+    """
+
+    for arg in [gy, gm, gd]:
+        if not isinstance(arg, int):
+            raise TypeError('All arguments must be int. No other type is acceptable.')
+
+    g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+
+    if (gm > 2):
+        gy2 = gy + 1
+    else:
+        gy2 = gy
+
+    days = 355666 + (365 * gy) + ((gy2 + 3) // 4) - ((gy2 + 99) // 100) + ((gy2 + 399) // 400) + gd + g_d_m[gm - 1]
+    jy = -1595 + (33 * (days // 12053))
+    days %= 12053
+    jy += 4 * (days // 1461)
+    days %= 1461
+
+    if (days > 365):
+        jy += (days - 1) // 365
+        days = (days - 1) % 365
+
+    if (days < 186):
+        jm = 1 + (days // 31)
+        jd = 1 + (days % 31)
+    else:
+        jm = 7 + ((days - 186) // 30)
+        jd = 1 + ((days - 186) % 30)
+
+    return [jy, jm, jd]
+
+
+def j2g(jy: int, jm: int, jd: int) -> List[int]:
+    """
+    Convert Jalali date to Gregorian.
+    The function to convert the Jalali date to Gregorian is taken from the following link:
+    https://jdf.scr.ir/jdf/python
+
+    :param jy: Jalali year: int
+    :param jm: Jalali month: int
+    :param jd: Jalali day: int
+    :return: List[[gregorian_year: int, gregorian_month: int, gregorian_day: int]]
+    """
+
+    for arg in [jy, jm, jd]:
+        if not isinstance(arg, int):
+            raise TypeError('All arguments must be int. No other type is acceptable.')
+
+    jy += 1595
+    days = -355668 + (365 * jy) + ((jy // 33) * 8) + (((jy % 33) + 3) // 4) + jd
+
+    if (jm < 7):
+        days += (jm - 1) * 31
+    else:
+        days += ((jm - 7) * 30) + 186
+
+    gy = 400 * (days // 146097)
+    days %= 146097
+    if (days > 36524):
+        days -= 1
+        gy += 100 * (days // 36524)
+        days %= 36524
+        if (days >= 365):
+            days += 1
+
+    gy += 4 * (days // 1461)
+    days %= 1461
+    if (days > 365):
+        gy += ((days - 1) // 365)
+        days = (days - 1) % 365
+    gd = days + 1
+
+    if ((gy % 4 == 0 and gy % 100 != 0) or (gy % 400 == 0)):
+        kab = 29
+    else:
+        kab = 28
+    sal_a = [0, 31, kab, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    gm = 0
+
+    while (gm < 13 and gd > sal_a[gm]):
+        gd -= sal_a[gm]
+        gm += 1
+
+    return [gy, gm, gd]
+
