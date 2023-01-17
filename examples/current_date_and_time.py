d017f7cdb10332c1dc216314abce39a003162ec3
mimseyedi <mim.seyedi@gmail.com>
2023-01-17 17:03:04 +0330

    Added current date and time

---
+++ examples/current_date_and_time.py
@@ -0,0 +1,7 @@
+from jaldt import now
+
+current_date_time_in_farsi = now()
+current_date_time_in_fingi = now(lang='fingilish')
+
+print(current_date_time_in_farsi)
+print(current_date_time_in_fingi)
